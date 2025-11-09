eb7bf3ec942dbe144abbe85b6d0404fb58f9ecf9
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-11-09 19:34:28 +0200

    Add blogs component manifest

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,5 +7,6 @@ require "decidim/conferences/test/factories" if Decidim.module_installed?(:confe
 require "decidim/initiatives/test/factories" if Decidim.module_installed?(:initiatives)
 require "decidim/participatory_processes/test/factories" if Decidim.module_installed?(:participatory_processes)
 
+require "decidim/blogs/test/factories" if Decidim.module_installed?(:blogs)
 require "decidim/meetings/test/factories" if Decidim.module_installed?(:meetings)
 require "decidim/proposals/test/factories" if Decidim.module_installed?(:proposals)
